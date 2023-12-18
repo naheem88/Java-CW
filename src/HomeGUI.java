2300aa0f98b007458c26cd470785b7cc5d1b5b96
ThuanNaheemPakeer <124042263+ThuanNaheemPakeer@users.noreply.github.com>
2023-12-18 05:35:24 +0530

    Java CW

--- src/HomeGUI.java
+++ src/HomeGUI.java
@@ -10,14 +10,16 @@ public class HomeGUI extends JFrame {
     JComboBox productTypeList;
     JButton cartBtn;
     ProductTableModel tableModel;
-    JPanel selectedProductPanel;
     JTable productTable;
     JScrollPane scrollPane;
+    JPanel selectedProductPanel;
     JTextArea textArea;
+    JButton addToShoppingCartBtn;
 
     private List<Product> productList;
     private List<Product> electronicList;
     private List<Product> clothingList;
+    private Product selectedProduct;
 
     public HomeGUI(WestminsterShoppingManager shopManager) {
         this.setTitle("Westminster Online Shopping Centre");
@@ -27,7 +29,7 @@ public class HomeGUI extends JFrame {
         menu = new JPanel();
 
         menu.add(new JLabel("Select Product Category"));
-        productTypeList = new JComboBox<>(new String[] {"All", "Electronics", "Clothing"});
+        productTypeList = new JComboBox<>(new String[]{"All", "Electronics", "Clothing"});
         menu.add(productTypeList);
         cartBtn = new JButton("Shopping Cart");
 
@@ -38,31 +40,30 @@ public class HomeGUI extends JFrame {
         electronicList = new ArrayList<>();
         clothingList = new ArrayList<>();
 
-        for (Product product: productList) {
+        for (Product product : productList) {
             if (product instanceof Electronic) {
                 electronicList.add(product);
-            } else if(product instanceof Clothing) {
+            } else if (product instanceof Clothing) {
                 clothingList.add(product);
             }
         }
 
         // Making table model
-        tableModel = new ProductTableModel(productList);
-        productTable = new JTable(tableModel);
-        scrollPane = new JScrollPane(productTable);
+        createTable(productList);
 
         // Selected Product Panel
         selectedProductPanel = new JPanel();
-        selectedProductPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-        textArea = new JTextArea(7, 1);
+        selectedProductPanel.setLayout(new FlowLayout());
+        textArea = new JTextArea(7,1);
+        textArea.setBackground(new Color(0, 0, 0, 0));
+        textArea.setEditable(false);
+        selectedProductPanel.add(textArea);
+        addToShoppingCartBtn = new JButton("Add to Shopping Cart");
 
         // Adding the components to the frame
         this.add(topPanel, BorderLayout.NORTH);
-        this.add(scrollPane, BorderLayout.CENTER);
         this.add(selectedProductPanel, BorderLayout.SOUTH);
-
         cartBtn.addMouseListener(new shpCartBtnHandler());
-        productTable.addMouseListener(new productSelectedHandler());
         productTypeList.addItemListener(new productTypeHandler());
     }
 
@@ -70,45 +71,65 @@ public class HomeGUI extends JFrame {
         public void mouseClicked(MouseEvent event) {
             ShoppingCartGUI shopCartFrame = new ShoppingCartGUI();
             // Settings for the frame
-            shopCartFrame.setSize(800,600);
+            shopCartFrame.setSize(800, 600);
             shopCartFrame.setVisible(true);
             shopCartFrame.setDefaultCloseOperation(shopCartFrame.DISPOSE_ON_CLOSE);
         }
     }
 
-    private class productSelectedHandler extends MouseAdapter  {
+    private class productSelectedHandler extends MouseAdapter {
         public void mouseClicked(MouseEvent event) {
-            selectedProductPanel.remove(textArea);
             int clickedRow = productTable.rowAtPoint(event.getPoint());
-            String productSelectedInfo = "Selected Product - Details\n" +
-                    productList.get(clickedRow).toString() + "\n" +
-                    productList.get(clickedRow).printProductInfo();
-            textArea.setText(productSelectedInfo);
-            textArea.setBackground(new Color(0, 0, 0, 0));
-            selectedProductPanel.add(textArea);
-        }
-    }
-
-    private class productTypeHandler implements ItemListener   {
-        public void itemStateChanged(ItemEvent event) {
-            if (event.getStateChange() == ItemEvent.SELECTED) {
-                if (productTypeList.getSelectedItem() == "All") {
-                    tableModel = new ProductTableModel(productList);
-                    productTable = new JTable(tableModel);
-                    scrollPane = new JScrollPane(productTable);
-                    add(scrollPane, BorderLayout.CENTER);
-                } else if (productTypeList.getSelectedItem() == "Electronics") {
-                    tableModel = new ProductTableModel(electronicList);
-                    productTable = new JTable(tableModel);
-                    scrollPane = new JScrollPane(productTable);
-                    add(scrollPane, BorderLayout.CENTER);
-                } else if (productTypeList.getSelectedItem() == "Clothing") {
-                    tableModel = new ProductTableModel(clothingList);
-                    productTable = new JTable(tableModel);
-                    scrollPane = new JScrollPane(productTable);
-                    add(scrollPane, BorderLayout.CENTER);
+            if (clickedRow != -1) {
+                Object productId = productTable.getModel().getValueAt(clickedRow, 0);
+                for (Product product : productList) {
+                    if (productId.equals(product.getProductId())) {
+                        String productSelectedInfo = "Selected Product - Details\n" + product + "\n" + product.printProductInfo();
+                        textArea.setText(productSelectedInfo);
+                        selectedProductPanel.add(addToShoppingCartBtn);
+                        selectedProduct = product;
+                        addToShoppingCartBtn.addMouseListener(new addToShoppingCartHandler());
+                        // Refreshes the JFrame
+                        HomeGUI.this.repaint();
+                        break;
+                    }
                 }
             }
         }
     }
+
+    private class productTypeHandler implements ItemListener {
+        public void itemStateChanged(ItemEvent event) {
+            if (event.getStateChange() == ItemEvent.SELECTED) {
+                HomeGUI.this.remove(scrollPane);
+                textArea.setText("");
+                selectedProductPanel.remove(addToShoppingCartBtn);
+                HomeGUI.this.repaint();
+                if (productTypeList.getSelectedItem() == "All") {
+                    createTable(productList);
+                } else if (productTypeList.getSelectedItem() == "Electronics") {
+                    createTable(electronicList);
+                } else if (productTypeList.getSelectedItem() == "Clothing") {
+                    createTable(clothingList);
+                }
+            }
+        }
+    }
+
+    private class addToShoppingCartHandler extends MouseAdapter {
+        public void mouseClicked(MouseEvent event) {
+            ShoppingCart userShoppingCart = new ShoppingCart();
+            userShoppingCart.addProduct(selectedProduct);
+        }
+    }
+
+    private void createTable(List<Product> productList) {
+        tableModel = new ProductTableModel(productList);
+        productTable = new JTable(tableModel);
+        scrollPane = new JScrollPane(productTable);
+        this.add(scrollPane, BorderLayout.CENTER);
+        productTable.addMouseListener(new productSelectedHandler());
+    }
 }
+
+
--- src/ShoppingCartGUI.java
+++ src/ShoppingCartGUI.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import java.awt.*;
 public class ShoppingCartGUI extends JFrame{
 
 }
